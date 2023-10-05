c375f55557bb91d4c902c8f3e9891e53b8be018a
Martin Abente Lahaye <martin.abente.lahaye@gmail.com>
2023-10-05 09:57:48 -0300

    tests: Add tests for all serializeable classes

---
+++ tests/test_serializeable.py
@@ -0,0 +1,59 @@
+from valley.common.action import Action
+from valley.common.entity import Entity, Vector
+from valley.common.heartbeat import Heartbeat
+from valley.common.message import Message
+from valley.common.scene import Scene
+from valley.common.session import Session, SessionRequest
+
+
+def test_serialize_vector():
+    original = Vector(x=45, y=45)
+    serialized = Vector.deserialize(original.serialize())
+
+    assert serialized.x == original.x
+    assert serialized.y == original.y
+
+
+def test_serialize_entity():
+    original = Entity(id=1, position=None, angle=0, velocity=0, action=Action.MOVE)
+    serialized = Entity.deserialize(original.serialize())
+
+    assert serialized.id == original.id
+    assert serialized.angle == original.angle
+    assert serialized.velocity == original.velocity
+    assert serialized.action == original.action
+
+
+def test_serialize_heartbeat():
+    original = Heartbeat(session_id=90)
+    serialized = Heartbeat.deserialize(original.serialize())
+
+    assert serialized.session_id == original.session_id
+
+
+def test_serialize_message():
+    original = Message(session_id=90, action=Action.MOVE, sequence=180)
+    serialized = Message.deserialize(original.serialize())
+
+    assert serialized.session_id == original.session_id
+    assert serialized.action == original.action
+    assert serialized.sequence == original.sequence
+
+
+def test_serialize_scene():
+    original = Scene(entities=None)
+    serialized = Scene.deserialize(original.serialize())
+
+    assert original.entities == serialized.entities
+
+
+def test_serialize_session():
+    original = Session(id=360)
+    serialized = Session.deserialize(original.serialize())
+
+    assert serialized.id == original.id
+
+
+def test_serialize_session_request():
+    original = SessionRequest()
+    SessionRequest.deserialize(original.serialize())
