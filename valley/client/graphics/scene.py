7f9ac15e2aec9a1e8f202a65b00bb3a96605f542
Martin Abente Lahaye <martin.abente.lahaye@gmail.com>
2023-10-05 17:20:16 -0300

    graphics: Allow scene viewer to rescale

--- valley/client/graphics/scene.py
+++ valley/client/graphics/scene.py
@@ -6,38 +6,34 @@ from ..game.scene import Scene as SceneModel
 
 
 class Scene(Gtk.DrawingArea):
-    TILES_X = 17
-    TILES_Y = 13
-    TILES_SIZE = 32
-
-    WIDTH = TILES_X * TILES_SIZE
-    HEIGHT = TILES_Y * TILES_SIZE
+    TILES_X = 16
+    TILES_Y = 16
 
     def __init__(self, model: Optional[SceneModel] = None) -> None:
         super().__init__()
 
         self._model = model
 
-        self.set_content_width(self.WIDTH)
-        self.set_content_height(self.HEIGHT)
         self.set_draw_func(self._draw_func, None)
 
     def _draw_func(self, widget, context, width, height, data=None):
         context.set_source_rgba(0, 0, 0)
-        context.rectangle(0, 0, self.WIDTH, self.HEIGHT)
+        context.rectangle(0, 0, width, height)
         context.fill()
 
         anchor = self._model.anchor
-        half_tile = self.TILES_SIZE / 2
-        half_width = self.WIDTH / 2
-        half_height = self.HEIGHT / 2
+
+        tile_width = width / self.TILES_X
+        tile_height = height / self.TILES_Y
+        offset_x = (width / 2) - (tile_width / 2)
+        offset_y = (height / 2) - (tile_height / 2)
 
         for entity in self._model.entities:
             context.set_source_rgba(1, 1, 1)
 
-            x = (entity.position.x - anchor.position.x) + half_width - half_tile
-            y = (entity.position.y - anchor.position.y) + half_height - half_tile
+            x = entity.position.x - anchor.position.x + offset_x
+            y = entity.position.y - anchor.position.y + offset_y
 
-            context.rectangle(x, y, self.TILES_SIZE, self.TILES_SIZE)
+            context.rectangle(x, y, tile_width, tile_height)
 
         context.fill()
