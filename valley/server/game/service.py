93f641fcdb8dc4671e3150ec8eb0c27f9a695ae1
Martin Abente Lahaye <martin.abente.lahaye@gmail.com>
2023-10-13 01:24:05 -0300

    server: Fix spawning point in multiplayer

--- valley/server/game/service.py
+++ valley/server/game/service.py
@@ -8,6 +8,7 @@ from ..network.tcp import Server as TCPServer
 from ..network.tcp import Client as TCPClient
 from ..network.udp import Server as UDPServer
 
+from ...common.entity import Vector
 from ...common.scanner import Description
 from ...common.utils import get_data_path
 from ...common.logger import logger
@@ -66,7 +67,15 @@ class Service(GObject.GObject):
     def __on_session_connected(self, manager, client, data):
         request = SessionRequest.deserialize(data)
 
-        entity_id = self.scene.add(request.type_id, position=self.scene.spawn)
+        entity_id = self.scene.add(
+            request.type_id,
+            position=Vector(
+                x=self.scene.spawn.x,
+                y=self.scene.spawn.y,
+                z=self.scene.spawn.z,
+            ),
+        )
+
         session = Session(id=self._index, entity_id=entity_id)
 
         self._index += 1
