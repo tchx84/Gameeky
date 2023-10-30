6180c9eb892206363348746cb8e15fc24115686d
Martin Abente Lahaye <martin.abente.lahaye@gmail.com>
2023-10-30 15:09:42 -0300

    client: Play sounds only during scene ticks

--- valley/client/sound/scene.py
+++ valley/client/sound/scene.py
@@ -11,7 +11,6 @@ class Scene:
 
         self._model = model
         self._model.connect("ticked", self.__on_scene_updated)
-        self._model.connect("updated", self.__on_scene_updated)
 
     def __on_scene_updated(self, model: SceneModel) -> None:
         if self._handler_id is None:
