48241f0eff39adf61b0474815e7a88e3c52d4282
Martin Abente Lahaye <martin.abente.lahaye@gmail.com>
2023-11-07 19:28:12 -0300

    editor: Add default stylesheet

--- valley/editor/main.py
+++ valley/editor/main.py
@@ -2,11 +2,14 @@
 
 import sys
 import gi
+import os
+
+__dir__ = os.path.dirname(os.path.abspath(__file__))
 
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
 
-from gi.repository import Gio, Adw
+from gi.repository import Gio, Adw, Gdk, Gtk
 
 from .widgets.window import Window
 
@@ -19,6 +22,14 @@ class Application(Adw.Application):
         )
 
     def do_activate(self) -> None:
+        css_provider = Gtk.CssProvider()
+        css_provider.load_from_path(os.path.join(__dir__, "style.css"))
+        Gtk.StyleContext.add_provider_for_display(
+            Gdk.Display.get_default(),
+            css_provider,
+            Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION,
+        )
+
         self._window = Window(application=self)
         self._window.present()
 
