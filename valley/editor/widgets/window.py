efa4df737b290b1b4ed6505a39e2eecd88022330
Martin Abente Lahaye <martin.abente.lahaye@gmail.com>
2023-11-09 11:10:07 -0300

    editor: Prepare window for integrated editor

--- valley/editor/widgets/window.py
+++ valley/editor/widgets/window.py
@@ -2,18 +2,41 @@ import os
 
 __dir__ = os.path.dirname(os.path.abspath(__file__))
 
-from gi.repository import Gtk, Adw
+from gi.repository import Gtk, Gio, Adw
 
-from .animation_row import AnimationRow
+from .entity_settings import EntitySettings
+
+from ...common.scanner import Description
 
 
 @Gtk.Template(filename=os.path.join(__dir__, "window.ui"))
 class Window(Adw.ApplicationWindow):
     __gtype_name__ = "Window"
 
-    content = Gtk.Template.Child()
+    game = Gtk.Template.Child()
 
     def __init__(self, *args, **kargs) -> None:
         super().__init__(*args, **kargs)
-        row = AnimationRow()
-        self.content.append(row)
+
+        self._entity_settings = EntitySettings()
+        self.game.props.child = self._entity_settings
+
+    def open(self, path: str) -> None:
+        description = Description.new_from_json(path)
+        self._entity_settings.description = description.game.default
+
+    def save(self, path: str) -> None:
+        description = Description(
+            game=Description(
+                default=self._entity_settings.description,
+            ),
+        )
+
+        file = Gio.File.new_for_path(path)
+        file.replace_contents(
+            contents=description.to_json().encode("UTF-8"),
+            etag=None,
+            make_backup=False,
+            flags=Gio.FileCreateFlags.REPLACE_DESTINATION,
+            cancellable=None,
+        )
