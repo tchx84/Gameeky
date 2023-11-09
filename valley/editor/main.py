a4240f3ddf629d0db1c74c75d69d0ce931c36eb4
Martin Abente Lahaye <martin.abente.lahaye@gmail.com>
2023-11-09 11:10:43 -0300

    editor: Add open and save actions

--- valley/editor/main.py
+++ valley/editor/main.py
@@ -9,9 +9,13 @@ __dir__ = os.path.dirname(os.path.abspath(__file__))
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
 
+from typing import Any, Optional
+
 from gi.repository import Gio, Adw, Gdk, Gtk
 
 from .widgets.window import Window
+
+from ..common.logger import logger
 
 
 class Application(Adw.Application):
@@ -20,6 +24,46 @@ class Application(Adw.Application):
             application_id="dev.tchx84.valley.Editor",
             flags=Gio.ApplicationFlags.NON_UNIQUE,
         )
+
+    def __on_open(self, action: Gio.SimpleAction, data: Optional[Any] = None) -> None:
+        json_filter = Gtk.FileFilter()
+        json_filter.add_pattern("*.json")
+
+        dialog = Gtk.FileDialog()
+        dialog.props.default_filter = json_filter
+        dialog.open(callback=self.__on_open_finished)
+
+    def __on_open_finished(
+        self,
+        dialog: Gtk.FileDialog,
+        result: Gio.AsyncResult,
+    ) -> None:
+        try:
+            file = dialog.open_finish(result)
+        except Exception as e:
+            logger.error(e)
+        else:
+            self._window.open(file.get_path())
+
+    def __on_save(self, action: Gio.SimpleAction, data: Optional[Any] = None) -> None:
+        json_filter = Gtk.FileFilter()
+        json_filter.add_pattern("*.json")
+
+        dialog = Gtk.FileDialog()
+        dialog.props.default_filter = json_filter
+        dialog.save(callback=self.__on_save_finished)
+
+    def __on_save_finished(
+        self,
+        dialog: Gtk.FileDialog,
+        result: Gio.AsyncResult,
+    ) -> None:
+        try:
+            file = dialog.save_finish(result)
+        except Exception as e:
+            logger.error(e)
+        else:
+            self._window.save(file.get_path())
 
     def do_activate(self) -> None:
         css_provider = Gtk.CssProvider()
@@ -35,6 +79,14 @@ class Application(Adw.Application):
 
     def do_startup(self) -> None:
         Adw.Application.do_startup(self)
+
+        open_action = Gio.SimpleAction.new("open", None)
+        open_action.connect("activate", self.__on_open)
+        self.add_action(open_action)
+
+        save_action = Gio.SimpleAction.new("save", None)
+        save_action.connect("activate", self.__on_save)
+        self.add_action(save_action)
 
     def do_shutdown(self) -> None:
         Adw.Application.do_shutdown(self)
