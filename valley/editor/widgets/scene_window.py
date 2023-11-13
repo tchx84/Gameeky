77adee415d4e54203f2002b7a0c6295e4a54116a
Martin Abente Lahaye <martin.abente.lahaye@gmail.com>
2023-11-13 16:14:49 -0300

    editor: Expand scene window with grid sidebar for entity lists

--- valley/editor/widgets/scene_window.py
+++ valley/editor/widgets/scene_window.py
@@ -4,7 +4,18 @@ __dir__ = os.path.dirname(os.path.abspath(__file__))
 
 from gi.repository import Gtk, Adw
 
+from .entity_row import EntityRow
+
+from ...common.scanner import Description
+
 
 @Gtk.Template(filename=os.path.join(__dir__, "scene_window.ui"))
 class SceneWindow(Adw.ApplicationWindow):
     __gtype_name__ = "SceneWindow"
+
+    entities = Gtk.Template.Child()
+
+    def register(self, description: Description) -> None:
+        entity = EntityRow()
+        entity.description = description
+        self.entities.append(entity)
