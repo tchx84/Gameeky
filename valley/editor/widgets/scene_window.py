d6af63dbdec46d4324b048e293cb8bb7aa11b132
Martin Abente Lahaye <martin.abente.lahaye@gmail.com>
2023-11-14 16:48:56 -0300

    editor: Extend scene window with slots for scene and grid viewers

--- valley/editor/widgets/scene_window.py
+++ valley/editor/widgets/scene_window.py
@@ -4,7 +4,11 @@ __dir__ = os.path.dirname(os.path.abspath(__file__))
 
 from gi.repository import Gtk, Adw
 
+from .scene import Scene as SceneView
+from .grid import Grid as GridView
 from .entity_row import EntityRow
+
+from ..models.scene import Scene as SceneModel
 
 from ...common.scanner import Description
 
@@ -14,8 +18,49 @@ class SceneWindow(Adw.ApplicationWindow):
     __gtype_name__ = "SceneWindow"
 
     entities = Gtk.Template.Child()
+    aspect = Gtk.Template.Child()
+    overlay = Gtk.Template.Child()
+
+    def __init__(self, *args, **kargs) -> None:
+        super().__init__(*args, **kargs)
+        self._scene_model = SceneModel()
+
+        self._scene_view = SceneView()
+        self._grid_view = GridView()
+        self._grid_view.connect("clicked", self.__on_clicked)
+
+        self.overlay.props.child = self._scene_view
+        self.overlay.add_overlay(self._grid_view)
+
+    def __on_clicked(self, grid: GridView, x: int, y: int) -> None:
+        selected = self.entities.get_selected_children()
+        if not selected:
+            return
+
+        child = selected[0].get_child()
+        if not child.type_id:
+            return
+
+        self._scene_model.add(child.type_id, x, y, 0)
 
     def register(self, description: Description) -> None:
         entity = EntityRow()
         entity.description = description
         self.entities.append(entity)
+
+    @property
+    def description(self) -> Description:
+        return Description()
+
+    @description.setter
+    def description(self, description: Description) -> None:
+        self._scene_model.description = description
+
+        self.aspect.props.ratio = self._scene_model.ratio
+
+        self._grid_view.columns = self._scene_model.width
+        self._grid_view.rows = self._scene_model.height
+        self._grid_view.scale = 1.0
+
+        self._scene_view.model = self._scene_model
+        self._scene_view.scale = 1.0
