30415700422fdaadefc7c557ee322b616c28a908
Martin Abente Lahaye <martin.abente.lahaye@gmail.com>
2023-12-11 19:05:46 -0300

    editor: Extend entitiy row model to keep track of path

--- src/gameeky/editor/models/entity_row.py
+++ src/gameeky/editor/models/entity_row.py
@@ -7,7 +7,8 @@ class EntityRow(GObject.GObject):
     type_id = GObject.Property(type=int)
     name = GObject.Property(type=str)
 
-    def __init__(self, type_id: int, name: str) -> None:
+    def __init__(self, type_id: int, name: str, path: str) -> None:
         super().__init__()
         self.type_id = type_id
         self.name = name
+        self.path = path
