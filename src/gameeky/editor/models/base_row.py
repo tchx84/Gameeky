399228ca488b59029232d17eabc6ec6fa28aaec7
Martin Abente Lahaye <martin.abente.lahaye@gmail.com>
2024-01-31 17:14:13 -0300

    editor: Provide actuators descriptions as tooltips

--- src/gameeky/editor/models/base_row.py
+++ src/gameeky/editor/models/base_row.py
@@ -26,25 +26,29 @@ class BaseRow(GObject.GObject):
     __gtype_name__ = "BaseRowModel"
 
     __items__: Dict[str, str] = {}
+    __tooltips__: Dict[str, str] = {}
 
     value = GObject.Property(type=str)
     text = GObject.Property(type=str)
+    tooltip = GObject.Property(type=str)
 
-    def __init__(self, value: str, text: str) -> None:
+    def __init__(self, value: str, text: str, tooltip: str) -> None:
         super().__init__()
         self.value = value
         self.text = text
+        self.tooltip = tooltip
 
     @classmethod
     def model(cls, default=False, exclude: Optional[List[str]] = None) -> Gio.ListStore:
         model = Gio.ListStore()
 
         if default is True:
-            model.append(cls(value="default", text=_("Default")))
+            model.append(cls(value="default", text=_("Default"), tooltip=""))
 
         for value, text in cls.__items__.items():
             if exclude is not None and value in exclude:
                 continue
-            model.append(cls(value=value, text=text))
+            tooltip = cls.__tooltips__.get(value, "")
+            model.append(cls(value=value, text=text, tooltip=tooltip))
 
         return model
