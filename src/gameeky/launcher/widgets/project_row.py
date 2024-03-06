c2076932e7e1b648a7547faad4eec9cb2bc7ae6e
Martin Abente Lahaye <martin.abente.lahaye@gmail.com>
2024-03-06 13:54:28 -0300

    launcher: Migrate ProjectRow to ProjectRowPopover

--- src/gameeky/launcher/widgets/project_row.py
+++ src/gameeky/launcher/widgets/project_row.py
@@ -16,14 +16,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import os
+from gi.repository import Gtk, GObject
 
-from gi.repository import Gtk, GLib, GObject
+from .project_row_popover import ProjectRowPopover
 
-from ...common.utils import launch_player, launch_scene, valid_file
-from ...common.monitor import Monitor
 from ...common.scanner import Description
-from ...common.definitions import DEFAULT_SCENE
 from ...common.config import VERSION
 
 
@@ -39,51 +36,18 @@ class ProjectRow(Gtk.FlowBoxChild):
 
     title = Gtk.Template.Child()
     subtitle = Gtk.Template.Child()
-    play = Gtk.Template.Child()
-    edit = Gtk.Template.Child()
-    settings = Gtk.Template.Child()
-    remove = Gtk.Template.Child()
+    options = Gtk.Template.Child()
 
     def __init__(self, path: str) -> None:
         super().__init__()
         self.path = path
 
-        writeable = GLib.access(self.path, os.W_OK) == 0
+        self._popover = ProjectRowPopover(self)
+        self._popover.set_parent(self.options)
 
-        self.edit.props.sensitive = writeable
-        self.settings.props.sensitive = writeable
-        self.remove.props.sensitive = writeable
-
-        self._monitor = Monitor()
-
-    def _get_project_path(self, *path) -> str:
-        return os.path.join(self.path, *path)
-
-    def _update_button(self) -> None:
-        self.play.props.sensitive = valid_file(self._get_project_path(DEFAULT_SCENE))
-
-    def __on_monitor_changed(self, monitor: Monitor) -> None:
-        self._update_button()
-
-    @Gtk.Template.Callback("on_play_clicked")
-    def __on_play_clicked(self, button: Gtk.Button) -> None:
-        launch_player(self.path, self._get_project_path(DEFAULT_SCENE))
-
-    @Gtk.Template.Callback("on_edit_clicked")
-    def __on_edit_clicked(self, button: Gtk.Button) -> None:
-        launch_scene(self.path, self._get_project_path(DEFAULT_SCENE))
-
-    @Gtk.Template.Callback("on_settings_clicked")
-    def __on_settings_clicked(self, button: Gtk.Button) -> None:
-        self.emit("edited")
-
-    @Gtk.Template.Callback("on_copy_clicked")
-    def __on_copy_clicked(self, button: Gtk.Button) -> None:
-        self.emit("copied")
-
-    @Gtk.Template.Callback("on_remove_clicked")
-    def __on_remove_clicked(self, button: Gtk.Button) -> None:
-        self.emit("removed")
+    @Gtk.Template.Callback("on_options_clicked")
+    def __on_options_clicked(self, button: Gtk.Button) -> None:
+        self._popover.popup()
 
     @property
     def description(self) -> Description:
@@ -97,13 +61,3 @@ class ProjectRow(Gtk.FlowBoxChild):
     def description(self, description: Description) -> None:
         self.title.props.label = description.name
         self.subtitle.props.label = description.description
-
-        self._monitor.shutdown()
-        self._monitor.add(self._get_project_path("scenes"))
-        self._monitor.connect("changed", self.__on_monitor_changed)
-
-        self._update_button()
-
-    def shutdown(self) -> None:
-        self._monitor.disconnect_by_func(self.__on_monitor_changed)
-        self._monitor.shutdown()
