2cfc30c824c9499d055c4e2ecd83a0c432009a4f
Martin Abente Lahaye <martin.abente.lahaye@gmail.com>
2024-03-07 13:45:07 -0300

    launcher: Add Importer and Exporter helpers to project model

--- src/gameeky/launcher/models/project.py
+++ src/gameeky/launcher/models/project.py
@@ -18,7 +18,13 @@
 
 import os
 import shutil
+import zipfile
 
+from typing import List, Tuple, Optional
+
+from gi.repository import GObject, GLib
+
+from ...common.logger import logger
 from ...common.scanner import Description
 from ...common.utils import get_projects_path, find_new_name
 
@@ -76,3 +82,148 @@ class Project:
     @classmethod
     def remove(cls, path: str) -> None:
         shutil.rmtree(path)
+
+
+class Exporter(GObject.GObject):
+    __gsignals__ = {
+        "started": (GObject.SignalFlags.RUN_LAST, None, ()),
+        "progressed": (GObject.SignalFlags.RUN_LAST, None, (float,)),
+        "finished": (GObject.SignalFlags.RUN_LAST, None, ()),
+        "failed": (GObject.SignalFlags.RUN_LAST, None, ()),
+    }
+
+    def __init__(self, source: str, target: str) -> None:
+        super().__init__()
+        self._total = 0
+        self._items: List[Tuple[str, str]] = []
+        self._source = source
+        self._target = target
+        self._zip: Optional[zipfile.ZipFile] = None
+
+    def start(self) -> None:
+        self._collect("gameeky.project")
+        self._collect("actuators")
+        self._collect("assets")
+        self._collect("entities")
+        self._collect("scenes")
+
+        try:
+            self._zip = zipfile.ZipFile(
+                self._target,
+                "w",
+                zipfile.ZIP_DEFLATED,
+                strict_timestamps=False,
+            )
+        except Exception as e:
+            logger.error(e)
+            self.emit("failed")
+        else:
+            self._total = len(self._items)
+            self.emit("started")
+            GLib.idle_add(self._do_step)
+
+    def _collect(self, name: str) -> None:
+        source = os.path.join(self._source, name)
+
+        self._items.append((source, name))
+
+        if os.path.isfile(source):
+            return
+
+        for root, dirs, files in os.walk(source):
+            for file in files:
+                self._items.append(
+                    (
+                        os.path.join(root, file),
+                        os.path.relpath(
+                            os.path.join(root, file),
+                            os.path.join(source, ".."),
+                        ),
+                    )
+                )
+
+    def _do_step(self) -> None:
+        if self._zip is None:
+            return
+
+        if not self._items:
+            self._finish()
+            return
+
+        try:
+            self._zip.write(*self._items.pop())
+        except Exception as e:
+            logger.error(e)
+            self.emit("failed")
+        else:
+            self.emit("progressed", 1.0 - len(self._items) / self._total)
+            GLib.idle_add(self._do_step)
+
+    def _finish(self) -> None:
+        if self._zip is not None:
+            self._zip.close()
+
+        self.emit("finished")
+
+
+class Importer(GObject.GObject):
+    __gsignals__ = {
+        "started": (GObject.SignalFlags.RUN_LAST, None, ()),
+        "progressed": (GObject.SignalFlags.RUN_LAST, None, (float,)),
+        "finished": (GObject.SignalFlags.RUN_LAST, None, ()),
+        "failed": (GObject.SignalFlags.RUN_LAST, None, ()),
+    }
+
+    def __init__(self, source: str, target: str) -> None:
+        super().__init__()
+        self._total = 0
+        self._items: List[str] = []
+        self._source = source
+        self._target = target
+        self._zip: Optional[zipfile.ZipFile] = None
+
+    def start(self) -> None:
+        try:
+            self._zip = zipfile.ZipFile(
+                self._source,
+                "r",
+                zipfile.ZIP_DEFLATED,
+                strict_timestamps=False,
+            )
+        except Exception as e:
+            logger.error(e)
+            self.emit("failed")
+        else:
+            self._collect(self._zip.namelist())
+            self.emit("started")
+            GLib.idle_add(self._do_step)
+
+    def _do_step(self) -> None:
+        if self._zip is None:
+            return
+
+        if not self._items:
+            self._finish()
+            return
+
+        try:
+            self._zip.extract(self._items.pop(), self._target)
+        except Exception as e:
+            logger.error(e)
+            self.emit("failed")
+        else:
+            self.emit("progressed", 1.0 - len(self._items) / self._total)
+            GLib.idle_add(self._do_step)
+
+    def _collect(self, names: List[str]) -> None:
+        for name in names:
+            if not os.path.isabs(name):
+                self._items.append(name)
+
+        self._total = len(self._items)
+
+    def _finish(self) -> None:
+        if self._zip is not None:
+            self._zip.close()
+
+        self.emit("finished")
